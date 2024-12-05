ec9d27af8aa14f573f5ca9a167f47ab446f33f21
Maykel <maykel.esser@loqusgroup.com>
2024-12-05 20:01:40 -0300

    pag em construção

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Caramelo
+
+O Caramelo é uma solução via aplicativo mobile para empreendedores e seus clientes abolirem de vez os cartõezinhos de papel que são utilizados para contabilizar créditos em troca de um benefício. Além da questão ambiental, o Caramelo centraliza todos seus cartões em um meio digital fácil, confiável e rápido.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 function Home() {
-    return <h1>Em construção</h1>;
+    return (
+        <>
+            <h1>Caramelo - App</h1>
+            <p>
+                O Caramelo é uma solução via aplicativo mobile para
+                empreendedores e seus clientes abolirem de vez os cartõezinhos
+                de papel que são utilizados para contabilizar créditos em troca
+                de um benefício. Além da questão ambiental, o Caramelo
+                centraliza todos seus cartões em um meio digital fácil,
+                confiável e rápido.
+            </p>
+            <p>Assim como o cachorro caramelo, o app é seu companheiro!</p>
+            <p>
+                <small>Em construção. Previsão - 2025</small>
+            </p>
+        </>
+    );
 }
 
 export default Home;
