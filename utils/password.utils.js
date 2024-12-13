70f642c2626ade9b48a6bc4e99a733df541d96e3
Maykel <maykel.esser@loqusgroup.com>
2024-12-13 17:44:36 -0300

    fix password utils to use module exports instead of instance every function

--- utils/password.utils.js
+++ utils/password.utils.js
@@ -1,39 +1,42 @@
-/**
- * @function passwordRequirementsRules
- * @author Maykel Esser
- * @description Get the password requirements rules
- * @returns {object[]} The password requirements rules
- */
-function passwordRequirementsRules() {
-    const requirements = [
-        { re: /.{6,}/, label: "Inclua ao menos 6 caracteres" },
-        { re: /[0-9]/, label: "Inclua um número" },
-        { re: /[a-z]/, label: "Inclua ao menos uma letra minúscula" },
-        { re: /[A-Z]/, label: "Inclua ao menos uma letra maiúscula" },
-        { re: /[$&+,:;=?@#|"<>.^*()%!-]/, label: "Inclua ao menos um caractere especial" },
-    ];
-    return requirements;
-}
+module.exports = {
 
-/**
- * @function getStrength
- * @author Maykel Esser
- * @description Get the password strength based on the requirements
- * @param {*} password
- * @returns {number} The password strength
- */
-function getStrength(password) {
+    /**
+     * @function passwordRequirementsRules
+     * @author Maykel Esser
+     * @description Get the password requirements rules
+     * @returns {object[]} The password requirements rules
+     */
+    passwordRequirementsRules: function () {
 
-    let multiplier = password.length > 5 ? 0 : 1;
-    let requirements = passwordRequirementsRules();
+        const requirements = [
+            { re: /.{6,}/, label: "Inclua ao menos 6 caracteres" },
+            { re: /[0-9]/, label: "Inclua um número" },
+            { re: /[a-z]/, label: "Inclua ao menos uma letra minúscula" },
+            { re: /[A-Z]/, label: "Inclua ao menos uma letra maiúscula" },
+            { re: /[$&+,:;=?@#|"<>.^*()%!-]/, label: "Inclua ao menos um caractere especial" },
+        ];
 
-    requirements.forEach((requirement) => {
-        if (!requirement.re.test(password)) {
-            multiplier += 1;
-        }
-    });
+        return requirements;
+    },
 
-    return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10);
-}
+    /**
+     * @function getStrength
+     * @author Maykel Esser
+     * @description Get the password strength based on the requirements
+     * @param {*} password
+     * @returns {number} The password strength
+     */
+    getStrength: function (password) {
 
-module.exports = { getStrength, passwordRequirementsRules };
+        let multiplier = password.length > 5 ? 0 : 1;
+        let requirements = module.exports.passwordRequirementsRules();
+
+        requirements.forEach((requirement) => {
+            if (!requirement.re.test(password)) {
+                multiplier += 1;
+            }
+        });
+
+        return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10);
+    }
+};
