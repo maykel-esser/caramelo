b882f9331551f7430fb30e789895e093ce08f2d8
Maykel <maykel.esser@loqusgroup.com>
2024-12-15 00:55:29 -0300

    new tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,23 @@
 import database from "infra/database";
 
 export default async function status(req, res) {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    return res.status(200).json({ status: 'ok' });
+
+    const updatedAt = new Date().toISOString();
+    const serverVersion = await database.query("SHOW server_version;");
+    const maxConnections = await database.query("SHOW max_connections;");
+    const usedConnections = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [process.env.POSTGRES_DB]
+    });
+
+    return res.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: serverVersion.rows[0].server_version,
+                max_connections: parseInt(maxConnections.rows[0].max_connections),
+                used_connections: parseInt(usedConnections.rows[0].count),
+            }
+        }
+    });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,56 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("GET Status Endpoint should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
-
     expect(response.status).toBe(200);
+});
+
+test("GET Status Endpoint should return an object containing updated_at, dependencies.database and the required fields", async () => {
+
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const body = await response.json();
+
+    expect(body).toEqual(
+        expect.objectContaining({
+            updated_at: expect.any(String),
+            dependencies: expect.objectContaining({
+                database: expect.objectContaining({
+                    version: expect.any(String),
+                    max_connections: expect.any(Number),
+                    used_connections: expect.any(Number),
+                }),
+            }),
+        }),
+    );
+});
+
+test("GET Status Endpoint should return an specific database version", async () => {
+
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const body = await response.json();
+
+    expect(body.dependencies.database.version).toBe("16.0");
+});
+
+test("GET Status Endpoint should return the same date as a dummy date", async () => {
+
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const body = await response.json();
+
+    const parsedUpdatedAt = new Date(body.updated_at).toISOString();
+    expect(body.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("GET Status Endpoint should return the same max_connections as 100", async () => {
+
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const body = await response.json();
+
+    expect(body.dependencies.database.max_connections).toEqual(100);
+});
+
+test("GET Status Endpoint should return the same used_connections as 1", async () => {
+
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const body = await response.json();
+
+    expect(body.dependencies.database.used_connections).toEqual(1);
 });
