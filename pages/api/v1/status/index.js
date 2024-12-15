77a6346b9089f0ddf24241485995a79ac2500b18
Maykel <maykel.esser@loqusgroup.com>
2024-12-14 22:03:18 -0300

    move infra

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,6 @@
-export default function status(req, res) {
+import database from "../../../../infra/database";
+
+export default async function status(req, res) {
+    const result = await database.query("SELECT 1 + 1 as sum;");
     res.status(200).json({ status: 'ok' });
 }
