7d5800c678beb4f74f5ccd419a2ada66a3ac46b8
Maykel <maykel.esser@loqusgroup.com>
2024-12-23 17:52:22 -0300

    logo lottie

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import Image from "next/image";
+import Lottie from "react-lottie";
 
 // Images
-import Logo from "/public/images/logos/logo-caramelo-vertical.png";
+import LogoAnimated from "/public/images/logos/logo-caramelo-lottie-text.json";
 
 export default function Page() {
+
+    const logoOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: LogoAnimated
+    };
+
     return (
         <>
             <div className="text-center p-16">
                 <div className="flex justify-center mb-20">
-                    <Image src={Logo} alt="®" width={200} />
+                    <Lottie options={logoOptions} height={200} width={200} />;
                 </div>
                 <p className="mb-8">O Caramelo® é um app para empreendedores e seus clientes abolirem de vez os cartõezinhos de papel que são utilizados para contabilizar créditos em troca de um benefício. Chega de carimbinho! Além da questão ambiental, o Caramelo® centraliza todos seus cartões em um meio digital fácil, confiável e rápido. Clientes podem buscar diretamente no appquais estabelecimentos aceitam Caramelo®!</p>
                 <p className="font-bold">Assim como o cachorro caramelo, ícone brasileiro, o app é seu companheiro! Fiel, assim como os clientes!</p>
