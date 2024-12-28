0590a7a43583bcad46bffbb708a1305ef419f02d
Maykel <maykel.esser@loqusgroup.com>
2024-12-28 19:49:56 -0300

    test: rename all descriptions to use new test standard

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,17 +7,16 @@ beforeAll(async () => {
 });
 
 describe("GET Migrations Endpoint", () => {
-    test("GET Migrations Endpoint should return 200", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations");
-        expect(response.status).toBe(200);
-    });
-
-    test("GET Migrations Endpoint should return an array", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations");
-        const responseBody = await response.json();
-
-        expect(Array.isArray(responseBody)).toBe(true);
-        expect(responseBody.length).toBeGreaterThan(0);
+    describe("Anonymous user", () => {
+        test("Running pending migrations", async () => {
+            const response = await fetch(
+                "http://localhost:3000/api/v1/migrations",
+            );
+            const responseBody = await response.json();
+            expect(response.status).toBe(200);
+            expect(Array.isArray(responseBody)).toBe(true);
+            expect(responseBody.length).toBeGreaterThan(0);
+        });
     });
 });
 
--- tests/unit/password.utils.test.js
+++ tests/unit/password.utils.test.js
@@ -1,35 +1,35 @@
 import { getStrength, passwordRequirementsRules } from "utils/password.utils";
 
 describe("getStrength", () => {
-    it("should return 10 for empty password", () => {
+    it("Should return score 10 for empty password", () => {
         const strength = getStrength("");
         expect(strength).toBe(10);
     });
 
-    it("should return strength for a weak password", () => {
+    it("Should return a score -50 for a weak password", () => {
         const strength = getStrength("12345");
         expect(strength).toBeLessThan(50);
     });
 
-    it("should return strength for a medium password", () => {
+    it("Should return strength between 50 and 100 for a medium password", () => {
         const strength = getStrength("12345aA");
         expect(strength).toBeGreaterThan(50);
         expect(strength).toBeLessThan(100);
     });
 
-    it("should return 100 for a strong password", () => {
+    it("Should return 100 for a strong password", () => {
         const strength = getStrength("12345aA!");
         expect(strength).toBe(100);
     });
 
-    it("should handle passwords shorter than 6 characters", () => {
+    it("Should handle passwords shorter than 6 characters as a low score", () => {
         const strength = getStrength("1aA!");
         expect(strength).toBeLessThan(67);
     });
 });
 
 describe("passwordRequirementsRules", () => {
-    it("should return an array with requirements, containing two fields each: re and label", () => {
+    it("Should return an array with requirements, containing two fields each: re and label", () => {
         const requirements = passwordRequirementsRules();
 
         expect(requirements).toBeInstanceOf(Array);
