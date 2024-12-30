40d7ef8c2c5270e1413a88a9cc63b06d0ae6e8fa
Maykel <maykel.esser@loqusgroup.com>
2024-12-30 01:12:24 -0300

    feat: install `framer-motion` to add mobile-like transitions

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,24 @@ import "@mantine/charts/styles.css";
 
 // Libs
 import { MantineProvider } from "@mantine/core";
+import { AnimatePresence, motion } from "framer-motion";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, router }) {
     return (
-        <MantineProvider>
-            <main>
-                <Component {...pageProps} />
-            </main>
-        </MantineProvider>
+        <AnimatePresence mode="wait" initial={false}>
+            <motion.div
+                key={router.route}
+                initial={{ x: "100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: "-100%", opacity: 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+                <MantineProvider>
+                    <main>
+                        <Component {...pageProps} />
+                    </main>
+                </MantineProvider>
+            </motion.div>
+        </AnimatePresence>
     );
 }
