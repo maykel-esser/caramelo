ab7f33a5a56066d0e1f9159836d492c7e5597a40
Maykel <maykel.esser@loqusgroup.com>
2025-01-02 23:55:26 -0300

    refactor(poc): adds `Link` to header's `Avatar`

--- components/pages/poc/header/index.js
+++ components/pages/poc/header/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import dynamic from "next/dynamic";
 import { Avatar } from "@mantine/core";
 
@@ -23,13 +24,15 @@ export default function Header(props) {
                 <div>
                     <Lottie options={logoOptions} height={55} width={55} />
                 </div>
-                <div className="relative">
-                    <Avatar
-                        src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
-                        alt="it's me"
-                        size="md"
-                    />
-                    <span className="absolute inset-0 object-right-top -mr-6 bg-red-500 text-center text-white w-3 h-3 rounded-full"></span>
+                <div>
+                    <Link className="relative" href="/poc/client/profile">
+                        <Avatar
+                            src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
+                            alt="Maykel Esser"
+                            size="md"
+                        />
+                        <span className="absolute inset-0 object-right-top -mr-6 bg-red-500 text-center text-white w-3 h-3 rounded-full"></span>
+                    </Link>
                 </div>
             </header>
         );
