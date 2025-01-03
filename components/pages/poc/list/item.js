83c69e2d98cabd17eddbc137a93d87f77fdb4f4a
Maykel <maykel.esser@loqusgroup.com>
2025-01-02 23:57:51 -0300

    feat(poc): adds new `ListItem` component

---
+++ components/pages/poc/list/item.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+// Icons
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+export default function ListItem(props) {
+    const Content = (
+        <div className="flex border-b py-4 items-center justify-between">
+            <div className="flex items-center gap-2">
+                {props.icon && <props.icon className="w-6" />}
+                <span>{props.title}</span>
+            </div>
+            {props.seeDetailsIcon && <ChevronRightIcon className="w-6" />}
+        </div>
+    );
+
+    return (
+        <li className={props.className}>
+            {props.href ? (
+                <Link href={props.href} className="block">
+                    {Content}
+                </Link>
+            ) : (
+                Content
+            )}
+        </li>
+    );
+}
