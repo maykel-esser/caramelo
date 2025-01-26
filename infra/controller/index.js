973248f71e68bb92beb61099735ab5ca930cd76c
Maykel <maykel.esser@loqusgroup.com>
2025-01-25 23:41:18 -0300

    feat(custom-errors): abstract controller infrastructure

---
+++ infra/controller/index.js
@@ -0,0 +1,25 @@
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+
+function onNoMatchHandler(req, res) {
+    const publicErrorObject = new MethodNotAllowedError();
+    return res.status(publicErrorObject.status_code).json(publicErrorObject);
+}
+
+function onErrorHandler(error, req, res) {
+    const publicErrorObject = new InternalServerError({
+        statusCode: error.status_code,
+        cause: error,
+    });
+    console.error(`Controller error:`);
+    console.error(publicErrorObject);
+    return res.status(publicErrorObject.status_code).json(publicErrorObject);
+}
+
+const controller = {
+    errorHandlers: {
+        onNoMatch: onNoMatchHandler,
+        onError: onErrorHandler
+    }
+}
+
+export default controller;
