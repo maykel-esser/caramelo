06b90f8526deb4b7843aec68d92fda045fb4e8e8
Maykel <maykel.esser@loqusgroup.com>
2025-01-25 23:40:36 -0300

    feat(custom-errors): add `ServiceError` and `MethodNotAllowedError`

--- infra/errors/index.js
+++ infra/errors/index.js
@@ -1,12 +1,51 @@
 export class InternalServerError extends Error {
-    constructor({ cause }) {
+    constructor({ cause, statusCode }) {
         super("An unexpected error occurred.", {
             cause,
         });
         this.name = "InternalServerError";
         this.action =
             "Try again in 5 minutes. If the error continues, please contact our support";
-        this.status_code = 500;
+        this.status_code = statusCode || 500;
+    }
+
+    toJSON() {
+        return {
+            name: this.name,
+            message: this.message,
+            action: this.action,
+            status_code: this.status_code,
+        };
+    }
+}
+
+export class ServiceError extends Error {
+    constructor({ cause, message }) {
+        super(message || "Service unavailable.", {
+            cause,
+        });
+        this.name = "InternalServerError";
+        this.action =
+            "Try again in 5 minutes. Check if the service is currently available";
+        this.status_code = 503;
+    }
+
+    toJSON() {
+        return {
+            name: this.name,
+            message: this.message,
+            action: this.action,
+            status_code: this.status_code,
+        };
+    }
+}
+
+export class MethodNotAllowedError extends Error {
+    constructor() {
+        super("Method not allowed to this endpoint");
+        this.name = "MethodNotAllowedError";
+        this.action = "Check if the HTTP method is valid for this endpoint";
+        this.status_code = 405;
     }
 
     toJSON() {
