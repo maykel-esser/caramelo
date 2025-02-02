cfac81e624ae9a88bc54acfcfa9244714d4cbac2
Maykel <maykel.esser@loqusgroup.com>
2025-02-01 23:35:03 -0300

    feat(poc): consumer profile

---
+++ pages/poc/consumer/index.js
@@ -0,0 +1,142 @@
+import Link from "next/link";
+import Header from "components/pages/poc/header";
+import NavigationMenu from "components/pages/poc/navigation";
+import { Avatar, Badge } from "@mantine/core";
+
+// Icons
+import {
+    MapPinIcon
+} from "@heroicons/react/24/outline";
+
+export default function Page() {
+
+    return (
+        <>
+            <NavigationMenu source="consumer" />
+            <main className="px-4 pb-32">
+                <Header source="consumer" />
+                <section className="mb-8 bg-white p-4 rounded-xl">
+                    <div className="flex justify-between items-center mb-4">
+                        <div>
+                            <h2 className="font-bold">Quase lá</h2>
+                            <p className="text-sm">Falta pouco pra fechar estes!</p>
+                        </div>
+                        <Link
+                            href="/poc/customer/credits"
+                            className="text-xs underline font-bold"
+                        >
+                            Meus cartões
+                        </Link>
+                    </div>
+                    <Link href="/poc/consumer/cards/details">
+                        <div className="flex gap-4 items-center mb-4 pb-4 border-b">
+                            <Avatar
+                                key="Wiegand Barber Shop"
+                                name="Wiegand Barber Shop"
+                                color="initials"
+                                size="md"
+                            />
+                            <div>
+                                <p className="text-xs font-bold">Wiegand Barber Shop</p>
+                                <p className="text-xs">A cada 10 cortes, ganhe um</p>
+                            </div>
+                            <div className="text-center ml-auto">
+                                <p className="text-xs">Faltam</p>
+                                <p className="text-lg font-bold">6</p>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href="/poc/consumer/cards/details">
+                        <div className="flex gap-4 items-center">
+                            <Avatar
+                                key="Luke Petshop"
+                                name="Luke Petshop"
+                                color="initials"
+                                size="md"
+                            />
+                            <div>
+                                <p className="text-xs font-bold">Luke Petshop</p>
+                                <p className="text-xs">A cada 10 banhos, ganhe um</p>
+                            </div>
+                            <div className="text-center ml-auto">
+                                <p className="text-xs">Faltam</p>
+                                <p className="text-lg font-bold">3</p>
+                            </div>
+                        </div>
+                    </Link>
+                </section>
+                <section className="mb-8 bg-white p-4 rounded-xl">
+                    <div className="flex justify-between items-center mb-4">
+                        <div>
+                            <h2 className="font-bold">Lugares próximos</h2>
+                            <p className="text-sm">Estes aqui aceitam Caramelo®</p>
+                        </div>
+                        <Link
+                            href="/poc/customer/explore"
+                            className="text-xs underline font-bold"
+                        >
+                            Explorar
+                        </Link>
+                    </div>
+                    <Link href="/poc/consumer/explore/details">
+                        <div className="flex gap-4 items-center mb-4 pb-4 border-b">
+                            <Avatar
+                                key="Luke Petshop"
+                                name="Luke Petshop"
+                                color="initials"
+                                size="md"
+                            />
+                            <div>
+                                <Badge color="black" size="xs" className="mb-2">Petshop</Badge>
+                                <p className="text-xs font-bold">Luke Petshop</p>
+                                <p className="text-xs">Estrada Guilherme Weigert 2245 - Santa Cândida</p>
+                            </div>
+                            <div className="text-center ml-auto flex gap-2 items-center">
+                                <MapPinIcon className="w-5 text-gray-400" />
+                                <p className="text-lg font-bold">3,5km</p>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href="/poc/consumer/explore/details">
+                        <div className="flex gap-4 items-center mb-4 pb-4 border-b">
+                            <Avatar
+                                key="Grano Vivo"
+                                name="Grano Vivo"
+                                color="initials"
+                                size="md"
+                            />
+                            <div>
+                                <Badge color="black" size="xs" className="mb-2">Pizzaria</Badge>
+                                <p className="text-xs font-bold">Grano Vivo</p>
+                                <p className="text-xs">Av. Paraná 3301 - Bacacheri</p>
+                            </div>
+                            <div className="text-center ml-auto flex gap-2 items-center">
+                                <MapPinIcon className="w-5 text-gray-400" />
+                                <p className="text-lg font-bold">4,5km</p>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href="/poc/consumer/explore/details">
+                        <div className="flex gap-4 items-center">
+                            <Avatar
+                                key="Wiegand Barber Shop"
+                                name="Wiegand Barber Shop"
+                                color="initials"
+                                size="md"
+                            />
+                            <div>
+                                <Badge color="black" size="xs" className="mb-2">Petshop</Badge>
+                                <p className="text-xs font-bold">Wiegand Barber Shop</p>
+                                <p className="text-xs">Av. Anita Garibaldi, 1928 - Barreirinha</p>
+                            </div>
+                            <div className="text-center ml-auto flex gap-2 items-center">
+                                <MapPinIcon className="w-5 text-gray-400" />
+                                <p className="text-lg font-bold">2,5km</p>
+                            </div>
+                        </div>
+                    </Link>
+                </section>
+            </main>
+        </>
+    );
+}
---
+++ pages/poc/consumer/profile/change-password/index.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import NavigationMenu from "components/pages/poc/navigation";
+import ActionHeader from "components/pages/poc/header/actionHeader";
+import { NewPasswordInput } from "components/pages/poc/form/password";
+import { Button, PasswordInput } from "@mantine/core";
+
+export default function Page() {
+    return (
+        <>
+            <NavigationMenu source="consumer" />
+            <main className="px-4 pb-32">
+                <ActionHeader
+                    title="Alterar sua senha"
+                    backToPreviousPage={true}
+                />
+                <section>
+                    <form>
+                        <div className="mb-4">
+                            <NewPasswordInput placeholder="Digite a nova senha" />
+                        </div>
+                        <div className="mb-8">
+                            <PasswordInput
+                                placeholder="Confirme sua nova senha"
+                                required
+                                size="md"
+                                radius="md"
+                            />
+                        </div>
+                        <Button
+                            variant="filled"
+                            fullWidth
+                            size="lg"
+                            radius="md"
+                            type="submit"
+                            color="black"
+                            component={Link}
+                            href="/poc/consumer/profile"
+                        >
+                            Atualizar dados
+                        </Button>
+                    </form>
+                </section>
+            </main>
+        </>
+    );
+}
