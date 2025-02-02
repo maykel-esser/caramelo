2c0151ba42b21644b3c51b2952184d6f25a96cfa
Maykel <maykel.esser@loqusgroup.com>
2025-02-01 23:34:05 -0300

    feat(poc): consumer navigation

--- components/pages/poc/header/index.js
+++ components/pages/poc/header/index.js
@@ -5,9 +5,6 @@ import { Avatar, Indicator } from "@mantine/core";
 // Dynamic imports (lottie need to run at client-side only)
 const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
-// Consts
-import { USER_ROLES } from "constants/roles.constants";
-
 // Images
 import LogoAnimated from "/public/images/logos/logo-caramelo-lottie.json";
 
@@ -18,25 +15,23 @@ export default function Header(props) {
         animationData: LogoAnimated,
     };
 
-    if (props.source === USER_ROLES.CLIENT) {
-        return (
-            <header className="flex justify-between pt-16 mb-8">
-                <div>
-                    <Lottie options={logoOptions} height={55} width={55} />
-                </div>
-                <div className="pt-2">
-                    <Link href="/poc/client/profile">
-                        <Indicator color="red" offset={5}>
-                            <Avatar
-                                alt="Maykel Esser"
-                                name="Maykel Esser"
-                                color="initials"
-                                size="md"
-                            />
-                        </Indicator>
-                    </Link>
-                </div>
-            </header>
-        );
-    }
+    return (
+        <header className="flex justify-between pt-16 mb-8">
+            <div>
+                <Lottie options={logoOptions} height={55} width={55} />
+            </div>
+            <div className="pt-2">
+                <Link href={`/poc/${props.source}/profile`}>
+                    <Indicator color="red" offset={5}>
+                        <Avatar
+                            alt="Maykel Esser"
+                            name="Maykel Esser"
+                            color="initials"
+                            size="md"
+                        />
+                    </Indicator>
+                </Link>
+            </div>
+        </header>
+    );
 }
--- components/pages/poc/navigation/index.js
+++ components/pages/poc/navigation/index.js
@@ -10,40 +10,73 @@ import {
     SquaresPlusIcon,
     UsersIcon as UsersOutlineIcon,
     BuildingStorefrontIcon,
+    UserCircleIcon
 } from "@heroicons/react/24/outline";
 
 export default function NavigationMenu(props) {
-    if (props.source === USER_ROLES.CLIENT) {
-        return (
-            <nav className="fixed bottom-0 bg-white w-full px-4 pb-8 pt-6 z-50 shadow-[0_-10px_10px_rgba(0,0,0,0.01)]">
-                <ul className="flex justify-around items-center">
-                    <NavigationMenuItem
-                        href="/poc/client"
-                        icon={ChartBarSquareIcon}
-                        label="Início"
-                    />
-                    <NavigationMenuItem
-                        href="/poc/client/cards"
-                        icon={TicketIcon}
-                        label="Cartões"
-                    />
-                    <NavigationMenuItem
-                        href="/poc/client/release"
-                        icon={SquaresPlusIcon}
-                        label="Liberar"
-                    />
-                    <NavigationMenuItem
-                        href="/poc/client/clients"
-                        icon={UsersOutlineIcon}
-                        label="Clientes"
-                    />
-                    <NavigationMenuItem
-                        href="/poc/client/profile/business-profile"
-                        icon={BuildingStorefrontIcon}
-                        label="Perfil"
-                    />
-                </ul>
-            </nav>
-        );
-    }
+
+    return (
+        <nav className="fixed bottom-0 bg-white w-full px-4 pb-8 pt-6 z-50 shadow-[0_-10px_10px_rgba(0,0,0,0.01)]">
+            <ul className="flex justify-around items-center">
+                {props.source === USER_ROLES.CLIENT && (
+                    <>
+                        <NavigationMenuItem
+                            href="/poc/client"
+                            icon={ChartBarSquareIcon}
+                            label="Início"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/client/cards"
+                            icon={TicketIcon}
+                            label="Cartões"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/client/release"
+                            icon={SquaresPlusIcon}
+                            label="Liberar"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/client/clients"
+                            icon={UsersOutlineIcon}
+                            label="Clientes"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/client/profile/business-profile"
+                            icon={BuildingStorefrontIcon}
+                            label="Perfil"
+                        />
+                    </>
+                )}
+                {props.source === USER_ROLES.CONSUMER && (
+                    <>
+                        <NavigationMenuItem
+                            href="/poc/consumer"
+                            icon={ChartBarSquareIcon}
+                            label="Início"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/consumer/cards"
+                            icon={TicketIcon}
+                            label="Cartões"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/consumer/retrieve"
+                            icon={SquaresPlusIcon}
+                            label="Resgatar"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/consumer/explore"
+                            icon={BuildingStorefrontIcon}
+                            label="Explorar"
+                        />
+                        <NavigationMenuItem
+                            href="/poc/consumer/profile"
+                            icon={UserCircleIcon}
+                            label="Perfil"
+                        />
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
 }
