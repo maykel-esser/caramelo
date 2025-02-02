72ec8547866f3e83b52530db2866072634276272
Maykel <maykel.esser@loqusgroup.com>
2025-02-02 01:19:57 -0300

    feat(poc): card details

---
+++ components/pages/poc/cards/clientCardList.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { Avatar, Badge } from "@mantine/core";
+
+// Consts
+import { RESOURCE_STATUS } from "constants/status.constants";
+
+// Icons
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+export default function ClientCardList(props) {
+    return (
+        <Link href="/poc/consumer/cards/details">
+            <div className="flex justify-between items-center mb-4 p-4 bg-white rounded-xl">
+                <div className="flex gap-4 items-center">
+                    <Avatar
+                        key={props.client}
+                        name={props.client}
+                        color="initials"
+                        size="lg"
+                    />
+                    <div>
+                        <Badge color="black" size="xs" className="">
+                            {props.category}
+                        </Badge>
+                        <h2 className="font-bold">{props.client}</h2>
+                        <p className="text-xs text-gray-400">{props.address}</p>
+                    </div>
+                </div>
+                <ChevronRightIcon
+                    className={
+                        "w-6" +
+                        (props.status === RESOURCE_STATUS.UNAVAILABLE
+                            ? " text-gray-300"
+                            : "")
+                    }
+                />
+            </div>
+        </Link>
+    );
+}
---
+++ pages/poc/consumer/cards/details/index.js
@@ -0,0 +1,115 @@
+import NavigationMenu from "components/pages/poc/navigation";
+import ActionHeader from "components/pages/poc/header/actionHeader";
+import ClientUserCard from "components/pages/poc/credits/clientUserCard";
+import { Badge } from "@mantine/core";
+
+// Icons
+import {
+    MapPinIcon,
+    BuildingStorefrontIcon,
+} from "@heroicons/react/24/outline";
+
+export default function Page() {
+    const dummyUserCardData = [
+        {
+            title: "A cada 10 serviços, ganhe um corte",
+            credit_needed: 10,
+            credits: [
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+            ],
+        },
+        {
+            title: "A cada 8 cervejas, ganhe uma",
+            credit_needed: 8,
+            credits: [
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+                {
+                    datetime: "2024-12-30T13:53:05.535Z",
+                },
+            ],
+        },
+    ];
+
+    return (
+        <>
+            <NavigationMenu source="consumer" />
+            <main className="px-4 pb-32">
+                <ActionHeader title="Cartões" backToPreviousPage={true} />
+                <section>
+                    <div>
+                        <h2 className="text-lg font-bold">
+                            Wiegand Barber Shop
+                        </h2>
+                        <p className="mb-2">
+                            Av. Anita Garibaldi, 1928 - Barreirinha
+                        </p>
+                        <div className="flex gap-2 items-center mb-8 pb-8 border-b">
+                            <Badge
+                                radius="sm"
+                                color="black"
+                                size="lg"
+                                leftSection={
+                                    <BuildingStorefrontIcon className="w-4" />
+                                }
+                            >
+                                Barbearia
+                            </Badge>
+                            <Badge
+                                radius="sm"
+                                color="black"
+                                size="lg"
+                                leftSection={<MapPinIcon className="w-4" />}
+                            >
+                                3.5km
+                            </Badge>
+                        </div>
+                    </div>
+                    <div>
+                        <ClientUserCard data={dummyUserCardData[0]} />
+                        <ClientUserCard data={dummyUserCardData[1]} />
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+}
