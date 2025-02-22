0352453b79bfbb86e580cc65f91c6c7f56266ded
Maykel <maykel.esser@loqusgroup.com>
2025-02-22 18:51:53 -0300

    feat: add `runPendingMigrations` method to `orchestrator`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -13,6 +13,7 @@
  */
 import retry from "async-retry";
 import database from "infra/database";
+import migrator from "models/migrator";
 
 /**
  * @function waitForAllServices
@@ -66,9 +67,14 @@ async function clearDatabase() {
     await database.query("CREATE SCHEMA public;");
 }
 
+async function runPendingMigrations() {
+    await migrator.runPendingMigrations();
+}
+
 const orchestrator = {
     waitForAllServices,
     clearDatabase,
+    runPendingMigrations,
 };
 
 export default orchestrator;
