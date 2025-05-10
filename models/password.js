8cec71f0c37fbac89eeb8c4d67f35d85c7cbc631
Maykel <maykel.esser@loqusgroup.com>
2025-05-10 11:32:53 -0300

    refactor: to add pepper logic into the `models/password`

--- models/password.js
+++ models/password.js
@@ -1,16 +1,23 @@
 import bcrypt from "bcryptjs";
 
 async function hash(password) {
+    if (password == undefined) {
+        throw new Error("Password is undefined");
+    }
     const rounds = getNumberOfRounds();
-    return await bcrypt.hash(password, rounds);
+    return await bcrypt.hash(addPepper(password), rounds);
 }
 
 async function compare(providedPassword, storedPassword) {
-    return await bcrypt.compare(providedPassword, storedPassword);
+    return await bcrypt.compare(addPepper(providedPassword), storedPassword);
 }
 
 function getNumberOfRounds() {
     return process.env.NODE_ENV === "production" ? 14 : 1;
+}
+
+function addPepper(password) {
+    return password + process.env.PEPPER;
 }
 
 const password = {
